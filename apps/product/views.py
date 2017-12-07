dd5370f346a6cf107cbcdadd083a840b5900a050
bkawan <bikeshkawang@gmail.com>
2017-12-07 18:41:07 +0545

    minor fixes

--- apps/product/views.py
+++ apps/product/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.views.generic import ListView, DetailView
 
+from apps.company.models import CompanyDetail
 from apps.product.models import Product
 
 
@@ -14,3 +15,8 @@ class ProductDetailView(DetailView) :
     model = Product
     template_name = 'product/detail.html'
     slug_field = 'slug'
+
+    def get_context_data(self, **kwargs) :
+        ctx = super().get_context_data(**kwargs)
+        ctx['company_detail'] = CompanyDetail.objects.first()
+        return ctx
