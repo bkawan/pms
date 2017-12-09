bde6a20017c8795681233a27f611ab05bbbb73a1
bkawan <bikeshkawang@gmail.com>
2017-12-09 12:03:04 +0545

    minor changes

--- config/settings/env.sample.py
+++ config/settings/env.sample.py
@@ -4,5 +4,15 @@
 from .prod import *
 # from .dev import *
 
-ALLOWED_HOSTS = ['lipipoint.com']
 # Other developer custom settings may go below in env.py
+
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.postgresql',
+        'NAME' : 'pmsnew',
+        'USER' : '',
+        'PASSWORD' : '',
+        'HOST' : 'localhost',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT' : '',  # Set to empty string for default.
+    }
+}
