bb067b46a3084e2cad4b297a3bd4637b3055480a
bkawan <bikeshkawang@gmail.com>
2018-02-24 07:32:01 +0545

    add user created limit and remove some field sets

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -5,6 +5,7 @@ from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth.models import Group
 from django.forms import forms
+from django.utils.translation import ugettext_lazy as _
 
 from apps.users.models import User
 
@@ -38,7 +39,13 @@ class MyUserAdmin(AuthUserAdmin):
     add_form = MyUserCreationForm
     fieldsets = (
                     ('User Profile', {'fields':['title', 'full_name', 'image', 'gender', ]}),
-                ) + AuthUserAdmin.fieldsets
+                    # ('Permissions', {'fields':('is_active', 'is_staff', 'is_superuser')}),
+                ) + (
+                    (None, {'fields':('username', 'password')}),
+                    (_('Personal info'), {'fields':('first_name', 'last_name', 'email')}),
+                    (_('Permissions'), {'fields':('is_active', 'is_staff', 'is_superuser')}),
+                    # (_('Important dates'), {'fields':('last_login', 'date_joined')}),
+                )
     list_display = ('id', 'username', 'full_name', 'is_superuser')
     search_fields = ('username', 'email', 'full_name')
     list_filter = ('is_staff', 'is_superuser', 'is_active')
@@ -46,6 +53,10 @@ class MyUserAdmin(AuthUserAdmin):
     def has_delete_permission(self, request, obj=None):
         return False
 
+    def has_add_permission(self, request, obj=None):
+        if User.objects.all().count() == 5:
+            return False
+        return True
 
-# admin.site.register(User)
+
 admin.site.unregister(Group)
